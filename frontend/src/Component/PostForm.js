f47aa565b8857a79dc884c2d670ae7b6859e69ce
libinggen <libinggengit@gmail.com>
2024-03-09 22:59:47 +0900

    change createPost

--- frontend/src/Component/PostForm.js
+++ frontend/src/Component/PostForm.js
@@ -1,6 +1,6 @@
 // frontend/src/Component/PostForm.js
-import { useMutation, gql } from '@apollo/client';
-import './PostForm.css'; 
+import { useMutation, gql } from "@apollo/client";
+import "./PostForm.css";
 
 const CREATE_POST = gql`
   mutation CreatePost($title: String!, $description: String!) {
@@ -16,34 +16,68 @@ const CREATE_POST = gql`
 
 export function PostForm() {
   let titleInput, descriptionInput;
-  const [createPost, { data, loading, error }] = useMutation(CREATE_POST, {
-    refetchQueries: [
-      'AllPosts',
-    ],
+  //   const [createPost, { data, loading, error }] = useMutation(CREATE_POST, {
+  //     refetchQueries: [
+  //       'AllPosts',
+  //     ],
+  //   });
+  const [createPost, { loading, error }] = useMutation(CREATE_POST, {
+    update(cache, { data: { createPost } }) {
+      cache.modify({
+        fields: {
+          listPosts(existingPostsRefs = [], { readField }) {
+            const newPostRef = cache.writeFragment({
+              data: createPost.post,
+              fragment: gql`
+                fragment NewPost on Post {
+                  id
+                  title
+                  description
+                }
+              `,
+            });
+            if (
+              existingPostsRefs.some(
+                (ref) => readField("id", ref) === createPost.post.id
+              )
+            ) {
+              return existingPostsRefs;
+            }
+            return [...existingPostsRefs, newPostRef];
+          },
+        },
+      });
+    },
   });
 
-  if (loading) return 'Submitting...';
+  if (loading) return "Submitting...";
   if (error) return `Submission error! ${error.message}`;
 
   return (
-    <div> 
-      <form className="form-container"
-        onSubmit={e => {
+    <div>
+      <form
+        className="form-container"
+        onSubmit={(e) => {
           e.preventDefault();
-          createPost({ variables: { title: titleInput.value, description: descriptionInput.value } });
-          titleInput.value = '';
-          descriptionInput.value = '';
+          createPost({
+            variables: {
+              title: titleInput.value,
+              description: descriptionInput.value,
+            },
+          });
+          titleInput.value = "";
+          descriptionInput.value = "";
         }}
       >
         <input
-          ref={node => {
+          ref={(node) => {
             titleInput = node;
           }}
           type="text"
           placeholder="Title"
         />
         <textarea
-          ref={node => {
+          ref={(node) => {
             descriptionInput = node;
           }}
           placeholder="Description"
