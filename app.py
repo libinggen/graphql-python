b3bbca538955e34a18eb94fc9b788dbde856188c
libinggen <libinggengit@gmail.com>
2024-03-09 21:07:22 +0900

    update delete

--- app.py
+++ app.py
@@ -11,9 +11,14 @@ from ariadne import load_schema_from_path, make_executable_schema, \
 from flask import request, jsonify
 from ariadne.explorer import ExplorerPlayground
 from api.queries import listPosts_resolver, getPost_resolver
-from api.mutations import create_post_resolver
-query = ObjectType("Query")
+from api.mutations import create_post_resolver, update_post_resolver, delete_post_resolver
+
 mutation = ObjectType("Mutation")
+mutation.set_field("createPost", create_post_resolver)
+mutation.set_field("updatePost", update_post_resolver)
+mutation.set_field("deletePost", delete_post_resolver)
+
+query = ObjectType("Query")
 query.set_field("listPosts", listPosts_resolver)
 query.set_field("getPost", getPost_resolver)
 mutation.set_field("createPost", create_post_resolver)
